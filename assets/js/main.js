5efd38c4e7f816618390054ac0c0ee8e2c5f5141
Rodrigo Anjos <rodrigo.kaos@ymail.com>
2018-08-07 11:58:17 -0300

    Remove old color selector

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,10 @@ var d = document,
 content = d.querySelector('.content');
 
 Array.from({ length: 1000 }).map(() => {
-	var el = d.createElement('div');
+	let el = d.createElement('div');
 	el.classList.add('pixel');
 	content.appendChild(el);
 });
-
-var pixels = Array.from(d.querySelectorAll('.pixel'));
 
 var colors = [
 	'black',
@@ -19,36 +17,45 @@ var colors = [
 	'white'
 ];
 
-activeColor = colors[0];
+let activeColor = colors[0];
 
 colors.map( color => {
-	var el = d.createElement('div');
+	let el = d.createElement('div');
 	el.classList.add(color);
 	el.addEventListener('mousedown', e => activeColor = el.className );
 	d.querySelector('.control').appendChild(el);	
 });
 
-pixels.map( el => {
+Array.from(d.querySelectorAll('.pixel'))
+	.map( el => {
 
-	let isPressed = false; //FIX
-	
-	var changeColor = () => { //FIX REMOVE OLD COLOR SELECTOR
-		if( isPressed ){
-			el.classList.add(activeColor);
-		}
-	};	
+		let isPressed = false; //FIX
+		
+		var changeColor = () => {
+			if( isPressed ){
+				let aux = el.classList.value.split(" ");
+				
+				if( aux.length > 1 && [aux].pop() != activeColor ){
+					aux.pop();
+					el.classList.value = aux.join(" ");
+				}
 
-	content.addEventListener('mousedown', e => {
-		isPressed = true;
-		el.addEventListener('mouseover', changeColor );
+				el.classList.add(activeColor);
+
+			}
+		};	
+
+		content.addEventListener('mousedown', e => {
+			isPressed = true;
+			el.addEventListener('mouseover', changeColor );
+		});
+		
+		content.addEventListener('mouseup', e => {
+			isPressed = false;
+			el.removeEventListener('mouseover', changeColor );		
+		});
+
 	});
-	
-	content.addEventListener('mouseup', e => {
-		isPressed = false;
-		el.removeEventListener('mouseover', changeColor );		
-	});
-
-});
 
 d.querySelector('.save').addEventListener('click', e => {
 
